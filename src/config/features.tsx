2f9009e233bf88ffa9b73dca0e628f7a74eed59f
digitalwareshub <74873288+digitalwareshub@users.noreply.github.com>
2025-05-22 12:56:08 +0530

    false

--- src/config/features.tsx
+++ src/config/features.tsx
@@ -1,3 +1,3 @@
 export const FEATURES = {
-    USER_AUTH_ENABLED: true, // Set to true when you want to enable user registration
+    USER_AUTH_ENABLED: false, // Set to true when you want to enable user registration
   };
